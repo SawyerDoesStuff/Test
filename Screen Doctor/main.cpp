67fa4cf2867d8696416d31ac5667582fb76181ee
SawyerDoesStuff <sawyerdoesstuff@gmail.com>
2017-04-23 20:17:09 -0700

    Removed KoalaT software, added Sawyer Sieja as creator.

--- Screen Doctor/main.cpp
+++ Screen Doctor/main.cpp
@@ -1,3 +1,8 @@
+//main.cpp
+/*
+This program will create a database for a small business.
+Created by Sawyer Sieja 2017
+*/
 #include <iostream>
 
 using namespace std;
@@ -5,6 +10,8 @@ using namespace std;
 int main()
 {
 	cout << "This is a test.";
+	int x;
+	cin >> x;
 
 	return 0;
 }
